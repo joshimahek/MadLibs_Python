1801f5d63b4a69a16989676904a886beaa57722b
joshimahek <joshimahek.05@gmail.com>
2025-01-05 20:48:56 +0530

    adding file

---
+++ Madlib(StringConcatenation).py
@@ -0,0 +1,23 @@
+
+print("Fill in some random words to read a CRAZY Story Based on your Inputs !!")
+month=input("Enter any month name : ")
+adverb=input("A adverb -ly : ")
+feeling1=input("A feeling : ")
+bodypart=input("A part of the body : ")
+personname=input("A persons name : ")
+feeling2=input("A Feeling/emotion : ")
+
+print("Story 1")
+story=(f"The bright daylight entered the room as I covered my face in the blanket. It was the month of {month}.{adverb} there was a loud knock on the door .{feeling1} I got up stretched my {bodypart} and moved closer to the door . When I opened the door I found {personname} waiting at the door. I was {feeling2} to see her")
+
+print(story)
+
+print("Story 2")
+
+season=input("Name a Season : ")
+adj1=input("Name a good Adjective : ")
+activity=input("Write a fav activity/hobby: ")
+adj2=input("Last, Put any Adjective and see a Paragraph :")
+
+para=f"The {season} has started. Its Starting to feel {adj1}. The People really enjoy {activity}.These Seasons are the proof we need to embrace Change and that is so {adj2}"
+print(para)
